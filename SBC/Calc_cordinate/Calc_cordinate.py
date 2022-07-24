ca30aae07a02419ca6fb3001560569dd9248e509
Jongsang Yoo <jamin0513@gmail.com>
2022-07-25 00:32:47 +0900

    지시기 자세정보 반영

--- SBC/Calc_cordinate/Calc_cordinate.py
+++ SBC/Calc_cordinate/Calc_cordinate.py
@@ -1,7 +1,12 @@
+import math
 import geopy
 import geopy.distance
 
-
+"""
+명진당 : 37.2221 / 127.1885
+5공학관 : 37.2221 / 127.1876
+명진당 ~ 5공 : 79.69 / 269.9997
+"""
 def calc_Cord(lat_ORG, lon_ORG ,bearing, mDistance):
     distance = 0.001 * mDistance
     
@@ -11,9 +16,23 @@ def calc_Cord(lat_ORG, lon_ORG ,bearing, mDistance):
     lat_TGT, lon_TGT = destination.latitude, destination.longitude
     return lat_TGT, lon_TGT
 
+def calc_Distance(pitch, Lidar):
+    theta = 90 - pitch
+    return Lidar * math.cos(math.pi * (theta / 180))
+
 def main():
-    lat_TGT, lon_TGT = calc_Cord(37.2225, 127.1873, 180, 40)
-    print(lat_TGT)
-    print(lon_TGT)
+    #Parsing Data Area
+    lat_org = 37.2221
+    lon_org = 127.1885
+    heading = 269.9997
+    pitch = 30
+    Lidar = 79.69 * 2
     
+    #calc new data
+    distance = calc_Distance(pitch, Lidar)
+    lat_TGT, lon_TGT = calc_Cord(lat_org, lon_org, heading, distance)
+    %
+    print(distance)
+    print(lat_TGT, lon_TGT)
+
 main()
