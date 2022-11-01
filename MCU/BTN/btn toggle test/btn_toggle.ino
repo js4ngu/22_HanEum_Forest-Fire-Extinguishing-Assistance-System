6045b73b9ec5dedbdf0d98ca2cd7242fcade1bcc
Jongsang Yoo <jamin0513@gmail.com>
2022-11-01 20:45:12 +0900

    btn toggle test

---
+++ MCU/BTN/btn toggle test/btn_toggle.ino
@@ -0,0 +1,29 @@
+bool BTN_toggle();
+
+//BTN
+bool current_btn = 0;
+bool prev_btn = 0;
+
+//LED
+bool LED_STATE = 1;
+
+void setup() {
+    pinMode(,INPUT);
+    pinMode(13,OUTPUT);
+    digitalWrite(13, HIGH);
+}
+
+void loop(){
+    if(BTN_toggle()){
+        LED_STATE = !LED_STATE;
+        digitalWrite(13, LED_STATE);
+    }
+}
+
+bool BTN_toggle(){
+    current_btn = digitalRead();
+    if ( (prev_btn ^ current_btn) == 1){
+        return 1;
+    }
+    prev_btn = current_btn;
+}
